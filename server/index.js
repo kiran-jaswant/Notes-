2afc095dc76ca0bbcd57f88ceef1969d6db90203
kiran-jaswant <kiranjaswant08@gmail.com>
2024-04-19 14:40:00 +0530

    port added

--- server/index.js
+++ server/index.js
@@ -129,6 +129,6 @@ app.get('/health',(req,res)=>{
 
 const PORT=process.env.PORT || 5000;
  
- app.listen(5000,()=>{
+ app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
  });
