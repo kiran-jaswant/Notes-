f3df149824d64d073da8ad47fbb855b828c21d37
kiran-jaswant <kiranjaswant08@gmail.com>
2024-04-19 14:46:34 +0530

    port corrected

--- server/index.js
+++ server/index.js
@@ -127,8 +127,8 @@ app.get('/health',(req,res)=>{
     })
 });
 
-const PORT=process.env.PORT || 5000;
- 
- app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
+const port = process.env.PORT || 5000;
+
+ app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
  });
